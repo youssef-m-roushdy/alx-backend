590f22a9e978738b45b1083eed4106ffb8d637df
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-18 00:11:04 +0300

    .

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+"""Task 1: Simple pagination.
+"""
 import csv
 import math
 from typing import List
-"""Task 1: Simple pagination.
-"""
+
 
 def index_range(page, page_size):
     """
