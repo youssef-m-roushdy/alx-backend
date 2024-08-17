484e0e55aed63d1d2d64ff674834c204d3de4942
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-17 23:57:50 +0300

    .

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """
+    Calculate the start and end indexes for pagination.
+    """
+    start_idx = (page - 1) * page_size
+    end_idx = page * page_size
+    return start_idx, end_idx
+  
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        ds = self.dataset()
+        page_range = index_range(page, page_size)
+        try:
+            return [self.__dataset[_] for _ in range(page_range[0], page_range[1])]
+        except IndexError:
+            return []
