5044142cee9a14031be8acfa82e8c40bf19c29ce
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-20 21:57:28 +0300

    .

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" FIFOCache module
+"""
+BasicCache = __import__('0-basic_cache').BasicCache
+
+
+class FIFOCache(BasicCache):
+    """ FIFOCache defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > self.MAX_ITEMS:
+            removed_key = next(iter(self.cache_data))
+            self.cache_data.pop(removed_key)
+            print("DISCARD: {}".format(removed_key))
