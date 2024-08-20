775f8cf581b1b1ed9f4f5b37cb05472451dd293a
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-21 00:01:36 +0300

    .

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -20,21 +20,21 @@ class LFUCache(BasicCache):
             return
 
         if key in self.cache_data:
-            self.lru_keys.remove(key)
+            self.lfu_keys.remove(key)
         elif len(self.cache_data) >= self.MAX_ITEMS:
-            removed_key = self.lru_keys.pop(0)
+            removed_key = self.lfu_keys.pop(0)
             del self.cache_data[removed_key]
             print("DISCARD: {}".format(removed_key))
 
         self.cache_data[key] = item
-        self.lru_keys.append(key)
+        self.lfu_keys.append(key)
 
     def get(self, key):
         """ Get an item by key
         """
         if key is None or key not in self.cache_data:
             return None
-        self.lru_keys.remove(key)
-        self.lru_keys.append(key)
+        self.lfu_keys.remove(key)
+        self.lfu_keys.append(key)
 
         return self.cache_data[key]
