710a298f21b4f0e96411832ad21ace3fb43180d6
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-21 00:45:18 +0300

    .

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -11,7 +11,7 @@ class LFUCache(BasicCache):
     """
     def __init__(self):
         super().__init__()
-        self.lfu_keys = []
+        self.lfu_dict = {}
 
     def put(self, key, item):
         """ Add an item in the cache
@@ -20,21 +20,25 @@ class LFUCache(BasicCache):
             return
 
         if key in self.cache_data:
-            self.lfu_keys.remove(key)
-        elif len(self.cache_data) >= self.MAX_ITEMS:
-            removed_key = self.lfu_keys.pop(0)
-            del self.cache_data[removed_key]
-            print("DISCARD: {}".format(removed_key))
+            self.lfu_dict[key] += 1
+        else:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                lfu = min(self.lfu_dict.items())
+                lfu_keys = [k for k, v in self.lfu_dict.items() if v == lfu]
+                if lfu_keys:
+                    removed_key = lfu_keys[0]
+                    del self.cache_data[removed_key]
+                    del self.lfu_dict[removed_key]
+                    print("DISCARD: {}".format(removed_key))
+            self.lfu_dict[key] = 1
 
         self.cache_data[key] = item
-        self.lfu_keys.append(key)
 
     def get(self, key):
         """ Get an item by key
         """
         if key is None or key not in self.cache_data:
             return None
-        self.lfu_keys.remove(key)
-        self.lfu_keys.append(key)
+        self.lfu_dict[key] += 1
 
         return self.cache_data[key]
