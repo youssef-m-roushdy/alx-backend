301e96f1d281ad64de74878a069309d8076f28ca
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-27 19:06:03 +0300

    .

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
