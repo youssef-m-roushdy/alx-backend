cc8c51eaad5ff9770f0a98db28bbb2fdb5c829c3
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-27 19:08:08 +0300

    .

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
-
+"""
+Flask app
+"""
 from flask import Flask, render_template
 from flask_babel import Babel
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    """
+    Return the home page content throw template 
+    """
     return render_template('0-index.html')
 
 if __name__ == '__main__':
