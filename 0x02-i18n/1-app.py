c0be5187cfb9d4597a3c233e65a9efcefc4a191e
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-27 19:34:35 +0300

    .

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,11 +2,23 @@
 """
 Flask app
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+babel = Babel(app)
 
 
 @app.route('/')
