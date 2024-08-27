26a89aa046cf90ca2ab45128ba38b41f79335b78
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-27 22:08:13 +0300

    .

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -2,7 +2,7 @@
 """
 Flask app
 """
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 
@@ -24,6 +24,14 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
 @babel.localeselector
 def get_locale():
     """
@@ -38,12 +46,33 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+def get_user(id):
+    """
+    Get user by his id if user found
+    """
+    if id in users:
+        return users[id]
+    return None
+
+
+@app.before_request
+def before_request():
+    """
+    Check user is logged in before request
+    """
+    user_id = request.args.get('login_as')
+    if user_id and user_id.isdigit():
+        g.user = get_user(int(user_id))
+    else:
+        g.user = None
+
+
 @app.route('/', strict_slashes=False)
 def home():
     """
     Return the home page content through the template
     """
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
